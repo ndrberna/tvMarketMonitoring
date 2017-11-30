5eae1f12a26734caf37abb1864fc0bfe98182af2
Andrea Bernardini <bernardini.andrea@gmail.com>
2017-11-30 15:54:00 +0100

    raw interface

--- dataViz/dataViz/urls.py
+++ dataViz/dataViz/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from marketPresence import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.mappa, name='mappa'),
+    url(r'^mappa', views.mappa, name='mappa'),
 ]
--- dataViz/marketPresence/views.py
+++ dataViz/marketPresence/views.py
@@ -2,5 +2,18 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+import csv
+from django.shortcuts import render_to_response, render, redirect
+from django.template.context import RequestContext
+from django.http import HttpResponse, HttpResponseRedirect
+from time import sleep
 
+import os
+from collections import Counter
+import math
+
+import time
 # Create your views here.
+def mappa(request):
+	
+	return render_to_response('marketPresence/mappa.html')
